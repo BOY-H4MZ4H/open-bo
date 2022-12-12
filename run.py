9dfc81a9891b4daf88edc46008cf16984ddb24d0
Boy Hamzah <88397313+BOY-H4MZ4H@users.noreply.github.com>
2022-12-12 20:27:46 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("sbf").main()
+       __import__("brute").Brute()
    except Exception as e:
        exit(str(e))
