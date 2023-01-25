0a848eb6173bb59c78f7be92afeb8d77906f29cb
Boy Hamzah <88397313+BOY-H4MZ4H@users.noreply.github.com>
2023-01-25 11:53:51 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("brutee").Brute()
+       __import__("main").Login()
    except Exception as e:
        exit(str(e))
